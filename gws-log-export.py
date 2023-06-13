24fb0024d879da1f8a1663e752c0cc5e201c37c2
Konrads Klints <kklints@kpmg.com.sg>
2023-06-13 13:30:57 +0800

    reasonable working version

--- gws-log-export.py
+++ gws-log-export.py
@@ -9,6 +9,11 @@ import requests
 from datetime import datetime, timedelta
 from pathlib import Path
 import ujson
+import sys
+import textwrap
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import signal
+from threading import Event
 def create_parser():
     """
     Function to create an argument parser for the Google Admin SDK reports_v1 API.
@@ -74,12 +79,26 @@ def create_parser():
         help="Directory where to output retrieved data, defaults to current directory if not provided",
     )
     parser.add_argument(
-        "--time-chunks",
+        "--interval",
         required=False,
         type=int,
-        default=60,
+        default=60*24*7,
         help="Time in minutes into which the logs will be split for retrieval",
     )
+    parser.add_argument(
+        "--user",
+        required=False,
+        type=str,
+        default='all',
+        help="User account to retrieve, defaults to all",
+    )
+    # parser.add_argument(
+    #     "--threads",
+    #     required=False,
+    #     type=int,
+    #     default=1,
+    #     help="Number of threads to use to retrieve data",
+    # )
 
     return parser
 
@@ -161,8 +180,8 @@ def get_intervals(start, end, interval):
     Returns:
         List[Tuple[datetime, datetime]]: List of tuples representing datetime pairs.
     """
-    start = datetime.fromisoformat(start)
-    end = datetime.fromisoformat(end)
+    # start = datetime.fromisoformat(start)
+    # end = datetime.fromisoformat(end)
     interval = timedelta(minutes=interval)
 
     pairs = []
@@ -174,9 +193,9 @@ def get_intervals(start, end, interval):
 
     return pairs
 
-def fetch_logs(service,app:str,start_time:datetime,end_time:datetime,user_key:str='all'):
+def fetch_logs(service,app:str,start_time:datetime,end_time:datetime,user_key,event=None):
     logger.info(f"Retrieving data for app {app} from {start_time.isoformat()} to {end_time.isoformat()} for user {user_key}")
-    logs=[]
+    # logs=[]
     activities = service.activities()
     request = activities.list(
         userKey=user_key, 
@@ -188,22 +207,38 @@ def fetch_logs(service,app:str,start_time:datetime,end_time:datetime,user_key:st
         # pageToken=page_token
     )
     while request is not None:
+        if event is not None and event.is_set():
+            logger.error("Caught exit event")
+            return None
         activities_doc = request.execute()
         items=activities_doc.get('items', [])
         logger.debug(f"Retrieved {len(items)} items, item timestamps are {items[-1]['id']['time']}-{items[0]['id']['time']}")
-        logs.extend(items)
+        for i in items:
+            yield i
         request = activities.list_next(request, activities_doc)
     logger.info(f"Completed retrieval for app {app} from {start_time.isoformat()} to {end_time.isoformat()} for user {user_key}")
-    return logs
+    # return logs
+
+def do_work(service,a,start,end,user,dest):            
+    with open(dest,"w",newline='') as f:
+        logs=fetch_logs(service,a,start,end,user)
+        for l in logs:
+            f.write(ujson.dumps(l)+"\n")
+
 
 def main():
     parser = create_parser()
+    for v in sys.argv:
+        if v == "--show-applications":
+                all_apps=get_valid_applications()
+                for a,d in all_apps.items():
+                    wrappde_d="\n".join(textwrap.wrap(d,width=50,subsequent_indent="\t\t"))
+                    print(f"{a}\t\t{wrappde_d}")
+                return
+
+
     args = parser.parse_args()
     all_apps=get_valid_applications()
-    if args.show_all_applications:
-        for a,d in all_apps.items():
-            print(f"{a}\t{d}")
-        return
     apps=[]
     if args.all_applications:
         apps=all_apps.keys()
@@ -221,15 +256,18 @@ def main():
     service = build('admin', 'reports_v1', credentials=credentials)
     # start_date=
     for a in apps:
-        start=args.start_date
-        end=args.end_date
-        dest=Path(args.output_directory) / f"{a}-{start.isoformat().replace(':','')}-{end.isoformat().replace(':','')}.json"
-        with open(dest,"w",newline='') as f:
-            logs=fetch_logs(service,a,start,end)
-            for l in logs:
-                f.write(ujson.dumps(l)+"\n")
+        # start=args.start_date
+        # end=args.end_date
+        futures=[]
+        for start,end in get_intervals(args.start_date,args.end_date,args.interval):
+            dest=Path(args.output_directory) / f"{args.user}-{a}-{start.isoformat().replace(':','')}-{end.isoformat().replace(':','')}.json"
+            do_work(service,a,start,end,args.user,dest)
 
 
 
 if __name__ == "__main__":
     main()
+
+
+
+# Notes: token will only retrieve for 180 days
